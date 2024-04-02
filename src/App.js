8f93bc60aadd6155263f59d4b8e0b1253e4aaccd
Murat GÜNDÜZ <96826984+muratgunduzz@users.noreply.github.com>
2024-04-02 03:48:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThermometerHalf, faCity, faCloud } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import City from "./City";
 
 function App() {
+  const key = "d6501e5664a072711d7c97760b83369f";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Enter city name"
+          className="input-field"
+        />
+        {city && (
+          <div className="city-info">
+            <h2>{city.name}</h2>
+            <div className="info-block">
+              <FontAwesomeIcon icon={faThermometerHalf} />
+              <p>Temperature: {city.main.temp} °C</p>
+            </div>
+            <div className="info-block">
+              <FontAwesomeIcon icon={faCity} />
+              <p>City: {city.name}</p>
+            </div>
+            <div className="info-block">
+              <FontAwesomeIcon icon={faCloud} />
+              <p>Weather: {city.weather[0].description}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
